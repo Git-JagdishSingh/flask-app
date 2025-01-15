8084b1e3207783e9f81faa19c76131f4752b8180
Git-JagdishSingh <163667194+Git-JagdishSingh@users.noreply.github.com>
2025-01-15 15:50:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hey Jagdish, this is DevOps site!"
+    return "changes made by github, this is DevOps site!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
